e75d7849a854a68a4b6c636257411595d629f0cc
vstres <126715443+vstres@users.noreply.github.com>
2023-03-10 01:07:22 -0300

    Update TrabalhoAlgotimosJogoFutibol.c

--- TrabalhoAlgotimosJogoFutibol.c
+++ TrabalhoAlgotimosJogoFutibol.c
@@ -1,41 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <iostream>
-#include <cstdlib>
-
-using namespace std;//gerar numeros aleatório 
-
-int main()
-{
- //Lista de Jogadores
- printf("*****************\n");
- printf("* Dados Jogador *\n");
- printf("*****************");
- int id = 1;
- char *nome1 ="Romario";
- char *sobrenome1 =" da Silva";
- int idade1 = 25;
- char *time1 = "Vasco";
- int valor = 10000000/100;//jogador vale 100.000
+#include <stdio.h>
+#include <string.h>
  
- printf("\n\nID: %d", id);
- printf("\nNome: %s%s", nome1, sobrenome1);
- printf("\nIdade: %d", idade1);
- printf("\nTime: %s", time1);
- printf("\nValor R$%.3d", valor);
-
-
- printf("\n\n\n*********************\n");
- printf("* Atributos Jogador *\n");
- printf("*********************");
+struct Dados {
+   int   ID ;
+   char  Nome[50];
+   char  Sobrenome[50];
+   int   Idade;
+   char  Time[30];
+   int   Valor;
+};
  
- 
-  srand(10); //gerar números aleatório 
-    cout<<rand()<<endl;
+void printDados( struct Dados DadosJogador );
 
-    return 0;
- 
+int main( ) {
 
- printf("\n\n\n");
- return 0;
+   struct Dados Dados1;        /* Declaramos Dados1 do tipo Dados */
+   struct Dados Dados2;        /* Declaramos Dados2 do tipo Dados */
+   
+ 
+   /* Dados do Jogador 1 */
+   Dados1.ID=1;
+   strcpy( Dados1.Nome, "Alex");
+   strcpy( Dados1.Sobrenome, "Lopes"); 
+   Dados1.Idade = 19;
+   strcpy( Dados1.Time, "Vasco");
+   Dados1.Valor = 10000000/100;
+  
+
+  /* Dados do Jogador 2 */
+   Dados2.ID=2;
+   strcpy( Dados2.Nome, "Stella");
+   strcpy( Dados2.Sobrenome, "Oliveira"); 
+   Dados2.Idade = 19;
+   strcpy( Dados2.Time, "Brasil");
+   Dados2.Valor = 20000000/100;
+   
+ 
+   /* Mostrando as Informações do Jogador 1 */
+   printDados( Dados1 );
+   
+   /* Mostrando as Informações do Jogador 1 */
+   printDados( Dados2 );
+
+   return 0;
+}
+
+void printDados( struct Dados DadosJogador ) {
+   printf("DADOS DO JOGADOR 1");
+   printf( "ID : %d\n", DadosJogador.ID);
+   printf( "NOME : %s\n",  DadosJogador.Nome);
+   printf( "Sobrenome : %s\n", DadosJogador.Sobrenome);
+   printf( "Idade : %d\n",DadosJogador.Idade);
+   printf( "Time : %s\n", DadosJogador.Time);
+   printf( "Valor : %d\n", DadosJogador.Valor);
+   printf("DADOS DO JOGADOR 2");
+   printf("\n");
+   
 }
