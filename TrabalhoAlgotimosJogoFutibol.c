e5f24d6c01e367f3583bce9af238b67fbe2fba70
vstres <126715443+vstres@users.noreply.github.com>
2023-03-19 18:19:34 -0300

    Update TrabalhoAlgotimosJogoFutibol.c

--- TrabalhoAlgotimosJogoFutibol.c
+++ TrabalhoAlgotimosJogoFutibol.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <string.h>
- 
+
+ // Tipo Boleano 
+/*typedef Força_de_Vontade; {
+  false,
+  true
+}
+
+typedef Criatividade; {
+  false,
+  true
+}
+
+typedef Lideranca; {
+  false,
+  true
+}*/
+
+//Declaramos as Variaveis Dados e Atributos do jogador
 struct Dados {
    int   ID ;
    char  Nome[50];
@@ -8,6 +25,26 @@ struct Dados {
    int   Idade;
    char  Time[30];
    int   Valor;
+   char  Informacoes[100];
+   char  Espaco[10];
+
+   //Variaveis dos Atributos
+
+int Força;
+
+int Velocidade;
+
+int resistência;
+
+//bool Força_de_Vontade;
+
+//bool Criatividade;
+
+//Bool Lideranca;
+
+//Char Posição[5];
+
+//bool Machucado;
 };
  
 void printDados( struct Dados DadosJogador );
@@ -19,22 +56,28 @@ int main( ) {
    
  
    /* Dados do Jogador 1 */
+   strcpy( Dados1.Informacoes, "DADOS DO JOGADOR 1");
    Dados1.ID=1;
    strcpy( Dados1.Nome, "Alex");
    strcpy( Dados1.Sobrenome, "Lopes"); 
    Dados1.Idade = 19;
    strcpy( Dados1.Time, "Vasco");
    Dados1.Valor = 10000000/100;
-  
+   strcpy( Dados1.Espaco, "\n");
+
+// Dados do atributos Jogador 1
+
+//Posição[5]{Gol, Defesa, Meio, Ataque, Líbero}; "Array"
 
   /* Dados do Jogador 2 */
+  strcpy( Dados2.Informacoes, "DADOS DO JOGADOR 2");
    Dados2.ID=2;
    strcpy( Dados2.Nome, "Stella");
    strcpy( Dados2.Sobrenome, "Oliveira"); 
    Dados2.Idade = 19;
    strcpy( Dados2.Time, "Brasil");
    Dados2.Valor = 20000000/100;
-   
+   strcpy( Dados2.Espaco, "\n");
  
    /* Mostrando as Informações do Jogador 1 */
    printDados( Dados1 );
@@ -46,13 +89,13 @@ int main( ) {
 }
 
 void printDados( struct Dados DadosJogador ) {
-   printf("DADOS DO JOGADOR 1");
+   printf( "%s\n", DadosJogador.Informacoes);
    printf( "ID : %d\n", DadosJogador.ID);
    printf( "NOME : %s\n",  DadosJogador.Nome);
    printf( "Sobrenome : %s\n", DadosJogador.Sobrenome);
    printf( "Idade : %d\n",DadosJogador.Idade);
    printf( "Time : %s\n", DadosJogador.Time);
    printf( "Valor : %d\n", DadosJogador.Valor);
-   printf("\n");
+   printf("%s\n", DadosJogador.Espaco);
    
 }
